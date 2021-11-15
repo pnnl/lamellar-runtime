aad489532eb1273895db40fa3b1d4ff8bec2c682
Ryan D. Friese <ryan.friese@pnnl.gov>
2021-11-15 11:51:08 -0800

    generic array example

---
+++ examples/array_examples/generic_array.rs
@@ -0,0 +1,23 @@
+use lamellar::array::{UnsafeArray,Distribution};
+use lamellar::{LamellarWorld,Dist};
+
+
+struct ArrayWrapper<T>{
+    array: UnsafeArray<T>
+}
+
+impl<T: Dist + Clone + 'static> ArrayWrapper<T>{
+    fn new(world: &LamellarWorld, len: usize) -> Self{
+        ArrayWrapper{
+            array: UnsafeArray::<T>::new(world,len,Distribution::Block)
+        }
+    }
+}
+
+fn main() {
+    let world = lamellar::LamellarWorldBuilder::new().build();
+    let my_pe = world.my_pe();
+    let _num_pes = world.num_pes();
+    let wrapped_array_f32 = ArrayWrapper::<f32>::new(&world,10);
+    let wrapped_array_usize = ArrayWrapper::<f32>::new(&world,10);
+}
