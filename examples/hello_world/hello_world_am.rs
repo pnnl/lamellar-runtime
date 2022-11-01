4e0c2db4091168daa44b745d03c7ce679058e2cc
Ryan D Friese <ryan.friese@pnnl.gov>
2022-10-31 19:56:17 -0700

    hello world examples

---
+++ examples/hello_world/hello_world_am.rs
@@ -0,0 +1,39 @@
+/// ------------Lamellar Example: Hello World-------------------------
+/// This example highlights how to create a Lamellar Active message
+/// the active message consists of a single input value (the pe id of the originating pe)
+/// when executed it will print Hello World, the PE and thread it executed on, and the orginating pe
+/// --------------------------------------------------------------------
+use lamellar::ActiveMessaging; // needed for exec_am_all
+
+
+//----------------- Hello World Active message -----------------//
+#[lamellar::AmData(Debug, Clone)]
+struct HelloWorld {
+    originial_pe: usize,
+}
+
+#[lamellar::am]
+impl LamellarAM for HelloWorld {
+    async fn exec(self) {
+        println!(
+            "Hello World  on PE {:?} of {:?} using thread {:?}, received from PE {:?}",
+            lamellar::current_pe,
+            lamellar::num_pes,
+            std::thread::current().id(),
+            self.originial_pe,
+        );
+    }
+}
+
+fn main() {
+    let world = lamellar::LamellarWorldBuilder::new().build();
+    let my_pe = world.my_pe();
+    world.barrier();
+
+    //Send a Hello World Active Message to all pes
+    let request = world.exec_am_all(HelloWorld {originial_pe: my_pe });
+
+    //wait for the request to complete
+    world.block_on(request);
+
+} //when world drops there is an implicit world.barrier() that occurs
---
+++ examples/hello_world/hello_world_array.rs
@@ -0,0 +1,37 @@
+/// ------------Lamellar Example: Hello World-------------------------
+/// This example highlights how to create a Lamellar Array
+/// as then perform simple add operations on its elements
+/// --------------------------------------------------------------------
+use lamellar::array::{ //using the array submodule
+    Distribution, // needed Distribution::{Block,Cyclic} 
+    ArithmeticOps, // needed for array.add(...);
+    AtomicArray // needed to construct AtomicArrays
+};
+
+
+fn main() {
+    let world = lamellar::LamellarWorldBuilder::new().build();
+    let num_pes = world.num_pes();
+    let my_pe = world.my_pe();
+    let local_length = 10;  //if you want to ensure each thread processes data make this >= LAMELLAR_THREADS environment variable
+    let global_length = num_pes * local_length;
+
+    let array = AtomicArray::<usize>::new(world.team(), global_length, Distribution::Block);
+
+    //print local data on each pe
+    array.print();
+    println!("");
+
+    //add 1 to each element of array
+    for i in 0..global_length{
+        array.add(i,1);
+    }
+    //wait for all the local add operations to finish
+    array.wait_all(); 
+    //wait for all the PEs to finish
+    array.barrier();
+
+    //print local data on each PE (should now be equal to num_pes)
+    array.print();
+    
+}
