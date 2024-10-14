c55d5fed676e9d96fba381962b6daa43223f077a
Ryan D. Friese <ryan.friese@pnnl.gov>
2024-10-14 11:25:30 -0700

    commit build.rs

---
+++ build.rs
@@ -0,0 +1,20 @@
+#[cfg(feature = "enable-rofi-shared")]
+use std::env;
+#[cfg(feature = "enable-rofi-shared")]
+use std::path::PathBuf;
+
+fn main() {
+    #[cfg(feature = "enable-rofi-shared")]
+    {
+        if let Ok(rofi_lib_dir) = env::var("DEP_ROFI_ROOT") {
+            let lib_path = PathBuf::from(rofi_lib_dir).join("lib");
+            println!("cargo:rustc-link-search=native={}", lib_path.display());
+            println!("cargo:rustc-link-arg=-Wl,-rpath,{}", lib_path.display());
+        } else {
+            panic!(
+                "unable to set rofi backend, recompile with 'enable-rofi' feature {:?}",
+                env::vars()
+            )
+        }
+    }
+}
