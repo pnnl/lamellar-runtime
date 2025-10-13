b68ccb72a40e6ffd97c859539cf5f4882ba801da
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2025-10-12 19:15:00 -0700

    1. Added pmix as vendored dependency (Will clone pmix instead of looking for one in the system) 2. Added prrte-sys also as a vendored dependency 3. Both of these -sys crates will pull their -sys dependencies as vendored 4. Introduced a lamellar_main proceedural macro and crate to automatically launch distributed sessions.

---
+++ src/lib.rs
@@ -0,0 +1,54 @@
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, token::Type, ItemConst, ItemFn};
+use quote::quote;
+
+#[proc_macro_attribute]
+pub fn lamellar_main(args: TokenStream, item: TokenStream) -> TokenStream {
+
+    // let mut source = item.clone().into_iter().peekable();
+    let func = parse_macro_input!(item as ItemFn);
+    assert!(func.sig.asyncness.is_none(), "async not supported");
+    assert!(func.sig.constness.is_none(), "const not supported");
+    assert!(func.sig.unsafety.is_none(), "unsafety not supported");
+    assert!(func.sig.generics.lt_token.is_none(), "generics not supported");
+    assert!(func.sig.variadic.is_none(), "variadics not supported");
+    assert!(func.sig.inputs.is_empty(), "inputs not supported");
+    let ret_result = match func.sig.output {
+        syn::ReturnType::Default => false,
+        syn::ReturnType::Type(_rarrow, ref t) => match t.as_ref() {
+            syn::Type::Path(ref type_path) => {assert!(type_path.path.segments.first().unwrap().ident.to_string() == "Result", "Only Result<(),...> is supported as return type"); true},
+            _other => panic!("Only Result<(),..> is supported as return type"),
+        },
+    };
+    let ret = if ret_result {Some(quote! {Ok(())})} else {None};
+    let _output = &func.sig.output;
+    let ret_type = func.sig.output;
+    let block = &func.block;
+
+    let res = quote! {
+        use prrte_sys::prterun_path;
+        use pmi::pmi::PmiBuilder;
+        use pmi::pmi::Pmi;
+        
+        fn main() #ret_type {
+            let prte_launched = std::env::var("PRTE_LAUNCHED").is_ok();
+            if !prte_launched {
+                let args: Vec<String> = std::env::args().collect();
+                let mut prterun_args = Vec::<String>::new();
+                for arg in args.iter().skip(1) {
+                    prterun_args.push(arg.to_string());
+                }
+                prterun_args.push(args[0].clone());
+                std::process::Command::new(prterun_path())
+                    .args(prterun_args)
+                    .status()
+                    .expect("failed to launch process");
+                #ret
+            }
+            else {
+                #block
+            }
+        } 
+    };
+    TokenStream::from(res)
+}
