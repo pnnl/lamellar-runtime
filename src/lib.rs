fb9f62cfc9d3fadce829f3fbdfc5012df6c15676
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2025-11-21 10:13:07 -0800

    Added lamellar_test in the lamellar_main crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, token::Type, ItemConst, ItemFn};
+use syn::{parse_macro_input, parse_quote, Attribute, ItemFn};
 use quote::quote;
 
 #[proc_macro_attribute]
-pub fn lamellar_main(args: TokenStream, item: TokenStream) -> TokenStream {
+pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
 
     // let mut source = item.clone().into_iter().peekable();
     let func = parse_macro_input!(item as ItemFn);
@@ -68,3 +68,97 @@ pub fn lamellar_main(args: TokenStream, item: TokenStream) -> TokenStream {
     };
     TokenStream::from(res)
 }
+
+#[proc_macro_attribute]
+pub fn lamellar_test(_args: TokenStream, item: TokenStream) -> TokenStream {
+
+    // let mut source = item.clone().into_iter().peekable();
+    let func = parse_macro_input!(item as ItemFn);
+    println!("Function name: {}", func.sig.ident.to_string());
+    assert!(func.sig.asyncness.is_none(), "async not supported");
+    assert!(func.sig.constness.is_none(), "const not supported");
+    assert!(func.sig.unsafety.is_none(), "unsafety not supported");
+    assert!(func.sig.generics.lt_token.is_none(), "generics not supported");
+    assert!(func.sig.variadic.is_none(), "variadics not supported");
+    assert!(func.sig.inputs.is_empty(), "inputs not supported");
+
+    let test_attr: Attribute = parse_quote!{
+        #[test]
+    };
+
+    let name = syn::Ident::new(format!("{}_launched", &func.sig.ident.to_string()).as_str(), func.sig.ident.span());
+    let vis = &func.vis;
+    let sig = &func.sig;
+
+
+    // let name = format!("{}_launched", name.to_string());
+    let res = quote! {
+        #test_attr #vis #sig {
+        fn type_name_of_val<T: ?Sized>(_val: &T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+        use prrte_sys::prterun_path;
+
+        let full_func_name = type_name_of_val(&#name);
+        let func_name = full_func_name
+            .split("::")
+            .into_iter()
+            .skip(1)
+            .collect::<Vec<&str>>()
+            .join("::");
+
+        let prte_launched = std::env::var("PRTE_LAUNCHED").is_ok();
+        if !prte_launched {
+        //     // Collect command line arguments
+            let mut args: Vec<String> = std::env::args().collect();
+
+        //     // Remove first argument (executable name) and maintain it for later
+            let exec = args.remove(0);
+
+        //     // Prepare arguments for prterun
+            let mut prterun_args = Vec::<String>::new();
+
+        //     // Collect any additional arguments after "--" to pass to prterun
+        //     let pos = args.iter().position(|x| x == "--");
+        //     if let Some(pos) = pos {
+        //         args.split_off(pos).into_iter().skip(1).for_each(|x| {
+        //             prterun_args.push(x.to_string());
+        //         });
+        //     }
+        //     let end = args.len();
+
+        //     // After the prterun arguments, add the executable name
+            prterun_args.push(exec);
+            prterun_args.push(func_name);
+            
+        //     // Add the arguments targeting the application
+            prterun_args.extend(args.into_iter());
+            prterun_args.push("--ignored".to_string());
+            prterun_args.push("--exact".to_string());
+            std::process::Command::new(prterun_path())
+                .args(prterun_args)
+                .status()
+                .expect("failed to launch process");
+        }
+        }
+    };
+
+        
+    let launched_attrs: Vec<Attribute> = parse_quote!{
+        #[test]
+        #[ignore]
+    };
+
+    let mut sig_launched = func.sig.clone();
+    sig_launched.ident = name;
+    let mut func_copy = func.clone();
+    func_copy.sig = sig_launched;
+    func_copy.attrs = launched_attrs;
+
+    let res = quote! {
+        #res
+        #func_copy
+    };
+    TokenStream::from(res)
+}
+
