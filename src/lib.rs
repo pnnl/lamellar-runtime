7cdb7c3dbd41f758ca8421f0f902e93e14d766a4
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2026-01-07 09:31:24 -0800

    [LAMELLA_MAIN] 1. Added compile-time checks to ensure one and only one launcher is enabled. 2. Added srun variant for lamellar_test 3. Cleaned up some code duplication

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@
 use proc_macro::TokenStream;
 // use proc_macro2::TokenStream;
 use syn::{parse_macro_input, parse_quote, Attribute, ItemFn};
-use quote::{quote, ToTokens};
+use quote::quote;
+#[cfg(any(feature = "use-prterun", feature = "use-srun"))]
+use quote::ToTokens;
+
 
 #[cfg(feature = "use-prterun")]
 fn launch_prterun(ret: Option<impl ToTokens>) -> impl ToTokens {
@@ -83,8 +86,11 @@ fn launch_srun(ret: Option<impl ToTokens>) -> impl ToTokens {
 
 #[proc_macro_attribute]
 pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
+    #[cfg(not(any(feature = "use-prterun", feature = "use-srun")))]
+    compile_error!("Either feature \"use-prterun\" or \"use-srun\" must be enabled for lamellar_main proc macro.");
+    #[cfg(all(feature = "use-prterun", feature = "use-srun"))]
+    compile_error!("Only one of features \"use-prterun\" or \"use-srun\" can be enabled for lamellar_main proc macro.");
 
-    // let mut source = item.clone().into_iter().peekable();
     let func = parse_macro_input!(item as ItemFn);
     assert!(func.sig.asyncness.is_none(), "async not supported");
     assert!(func.sig.constness.is_none(), "const not supported");
@@ -92,6 +98,8 @@ pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
     assert!(func.sig.generics.lt_token.is_none(), "generics not supported");
     assert!(func.sig.variadic.is_none(), "variadics not supported");
     assert!(func.sig.inputs.is_empty(), "inputs not supported");
+    
+    #[cfg(any(feature = "use-prterun", feature = "use-srun"))]
     let ret_result = match func.sig.output {
         syn::ReturnType::Default => false,
         syn::ReturnType::Type(_rarrow, ref t) => match t.as_ref() {
@@ -99,16 +107,20 @@ pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
             _other => panic!("Only Result<(),..> is supported as return type"),
         },
     };
+
+    #[cfg(any(feature = "use-prterun", feature = "use-srun"))]
     let ret = if ret_result {Some(quote! {Ok(())})} else {None};
     let _output = &func.sig.output;
     let ret_type = func.sig.output;
     let block = &func.block;
 
+    #[cfg(not(any(feature = "use-prterun", feature = "use-srun")))]
     let launch_block = quote! {
         if true {
             panic!("No launch method selected");
         }
     };
+    
 
     #[cfg(feature = "use-prterun")]
     let launch_block = launch_prterun(ret);
@@ -116,7 +128,9 @@ pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
     #[cfg(feature = "use-srun")]
     let launch_block = launch_srun(ret);
     
+    #[cfg(not(feature = "use-prterun"))]
     let import = quote! {};
+
     #[cfg(feature = "use-prterun")]
     let import = quote! {
         use prrte_sys::prterun_path;
@@ -135,30 +149,9 @@ pub fn lamellar_main(_args: TokenStream, item: TokenStream) -> TokenStream {
     TokenStream::from(res)
 }
 
-#[proc_macro_attribute]
-pub fn lamellar_test(_args: TokenStream, item: TokenStream) -> TokenStream {
-
-    // let mut source = item.clone().into_iter().peekable();
-    let func = parse_macro_input!(item as ItemFn);
-    println!("Function name: {}", func.sig.ident.to_string());
-    assert!(func.sig.asyncness.is_none(), "async not supported");
-    assert!(func.sig.constness.is_none(), "const not supported");
-    assert!(func.sig.unsafety.is_none(), "unsafety not supported");
-    assert!(func.sig.generics.lt_token.is_none(), "generics not supported");
-    assert!(func.sig.variadic.is_none(), "variadics not supported");
-    assert!(func.sig.inputs.is_empty(), "inputs not supported");
-
-    let test_attr: Attribute = parse_quote!{
-        #[test]
-    };
-
-    let name = syn::Ident::new(format!("{}_launched", &func.sig.ident.to_string()).as_str(), func.sig.ident.span());
-    let vis = &func.vis;
-    let sig = &func.sig;
-
-
-    // let name = format!("{}_launched", name.to_string());
-    let res = quote! {
+#[cfg(feature = "use-prterun")]
+fn launch_prterun_test(test_attr: Attribute, vis: &syn::Visibility, sig: &syn::Signature, name: &syn::Ident) -> impl ToTokens {
+    quote! {
         #test_attr #vis #sig {
         fn type_name_of_val<T: ?Sized>(_val: &T) -> &'static str {
             std::any::type_name::<T>()
@@ -175,16 +168,16 @@ pub fn lamellar_test(_args: TokenStream, item: TokenStream) -> TokenStream {
 
         let prte_launched = std::env::var("PRTE_LAUNCHED").is_ok();
         if !prte_launched {
-        //     // Collect command line arguments
+            // Collect command line arguments
             let mut args: Vec<String> = std::env::args().collect();
 
-        //     // Remove first argument (executable name) and maintain it for later
+            // Remove first argument (executable name) and maintain it for later
             let exec = args.remove(0);
 
-        //     // Prepare arguments for prterun
+            // Prepare arguments for prterun
             let mut prterun_args = Vec::<String>::new();
 
-        //     // Collect any additional arguments after "--" to pass to prterun
+            // Collect any additional arguments after "--" to pass to prterun
             let pos = args.iter().position(|x| x == "--");
             if let Some(pos) = pos {
                 args.split_off(pos).into_iter().skip(1).for_each(|x| {
@@ -210,7 +203,109 @@ pub fn lamellar_test(_args: TokenStream, item: TokenStream) -> TokenStream {
                 .expect("failed to launch process");
         }
         }
+    }
+}
+
+#[cfg(feature = "use-srun")]
+fn launch_srun_test(test_attr: Attribute, vis: &syn::Visibility, sig: &syn::Signature, name: &syn::Ident) -> impl ToTokens {
+    quote! {
+
+        #test_attr #vis #sig {
+        fn type_name_of_val<T: ?Sized>(_val: &T) -> &'static str {
+            std::any::type_name::<T>()
+        }
+
+        let full_func_name = type_name_of_val(&#name);
+        let func_name = full_func_name
+            .split("::")
+            .into_iter()
+            .skip(1)
+            .collect::<Vec<&str>>()
+            .join("::");
+
+        let slurm_launched = std::env::var("SLURM_LOCALID").is_ok();
+        if !slurm_launched {
+            use std::process::Stdio;
+            // Collect command line arguments
+            let mut args: Vec<String> = std::env::args().collect();
+
+            // Remove first argument (executable name) and maintain it for later
+            let exec = args.remove(0);
+
+            // Prepare arguments for prterun
+            let mut slurmrun_args = Vec::<String>::new();
+
+            // Collect any additional arguments after "--" to pass to prterun
+            let pos = args.iter().position(|x| x == "--");
+            if let Some(pos) = pos {
+                args.split_off(pos).into_iter().skip(1).for_each(|x| {
+                    slurmrun_args.push(x.to_string());
+                });
+            }
+            let end = args.len();
+
+            // After the prterun arguments, add the executable name
+            slurmrun_args.push(exec);
+            slurmrun_args.push(func_name);
+            
+            // Add the arguments targeting the application
+            slurmrun_args.extend(args.into_iter());
+            slurmrun_args.push("--ignored".to_string());
+            slurmrun_args.push("--exact".to_string());
+            // slurmrun_args.iter().for_each(|x| {
+            //     println!("prterun arg: {}", x);
+            // });
+            std::process::Command::new("srun")
+                .args(slurmrun_args)
+                .stdout(Stdio::null())
+                .stderr(Stdio::null())
+                .status()
+                .expect("failed to launch process");
+        }
+        }
+    }
+}
+
+#[proc_macro_attribute]
+pub fn lamellar_test(_args: TokenStream, item: TokenStream) -> TokenStream {
+    #[cfg(not(any(feature = "use-prterun", feature = "use-srun")))]
+    compile_error!("Either feature \"use-prterun\" or \"use-srun\" must be enabled for lamellar_test proc macro.");
+    #[cfg(all(feature = "use-prterun", feature = "use-srun"))]
+    compile_error!("Only one of features \"use-prterun\" or \"use-srun\" can be enabled for lamellar_test proc macro.");
+
+    let func = parse_macro_input!(item as ItemFn);
+    assert!(func.sig.asyncness.is_none(), "async not supported");
+    assert!(func.sig.constness.is_none(), "const not supported");
+    assert!(func.sig.unsafety.is_none(), "unsafety not supported");
+    assert!(func.sig.generics.lt_token.is_none(), "generics not supported");
+    assert!(func.sig.variadic.is_none(), "variadics not supported");
+    assert!(func.sig.inputs.is_empty(), "inputs not supported");
+
+    #[cfg(any(feature = "use-prterun", feature = "use-srun"))]
+    let test_attr: Attribute = parse_quote!{
+        #[test]
     };
+
+    let name = syn::Ident::new(format!("{}_launched", &func.sig.ident.to_string()).as_str(), func.sig.ident.span());
+    #[cfg(any(feature = "use-prterun", feature = "use-srun"))]
+    let vis = &func.vis;
+    #[cfg(any(feature = "use-prterun", feature = "use-srun"))]
+    let sig = &func.sig;
+
+
+    #[cfg(not(any(feature = "use-prterun", feature = "use-srun")))]
+    let res = quote! {
+        if true {
+            panic!("No launch method selected");
+        }
+    };
+
+    // let name = format!("{}_launched", name.to_string());
+    #[cfg(feature = "use-prterun")]
+    let res = launch_prterun_test(test_attr, vis, sig, &name);
+
+    #[cfg(feature = "use-srun")]
+    let res = launch_srun_test(test_attr, vis, sig, &name);
 
         
     let launched_attrs: Vec<Attribute> = parse_quote!{
