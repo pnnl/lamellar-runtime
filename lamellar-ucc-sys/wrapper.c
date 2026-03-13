5ee53aa460072e837b645815c320ae568555b974
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2026-03-12 19:21:29 -0700

    Remove lamellar-ucc-sys from repo

--- lamellar-ucc-sys/wrapper.c
+++
@@ -1,6 +0,0 @@
-#include "wrapper.h"
-
-ucc_status_t ucc_collective_test_wrapper(ucc_coll_req_h request)
-{
-    return ucc_collective_test(request);
-}
--- lamellar-ucc-sys/wrapper.h
+++
@@ -1,3 +0,0 @@
-#include <ucc/api/ucc.h>
-
-ucc_status_t ucc_collective_test_wrapper(ucc_coll_req_h request);
