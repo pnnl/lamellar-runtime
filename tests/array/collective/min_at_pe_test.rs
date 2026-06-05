1720cb872a8ef7b357f22c15f7a83eb7b0b717fc
Polykarpos Thomadakis <polykarpos.thomadakis@pnnl.gov>
2026-06-04 23:09:22 -0700

    Cleanup warnings in tests

--- tests/array/collective/min_at_pe_test.rs
+++ tests/array/collective/min_at_pe_test.rs
@@ -1,21 +1,6 @@
 use lamellar::array::prelude::*;
 use lamellar::memregion::prelude::*;
 
-fn initialize_mem_region<T: Dist + std::ops::AddAssign + std::ops::Mul<Output = T>>(
-    memregion: &LamellarMemoryRegion<T>,
-    init_val: T,
-    inc_val: T,
-    my_pe: T,
-) {
-    unsafe {
-        let mut i = init_val; //(len_per_pe * my_pe as f32).round() as usize;
-        for elem in memregion.as_mut_slice() {
-            *elem = i * my_pe;
-            i += inc_val;
-        }
-    }
-}
-
 macro_rules! initialize_array {
     (UnsafeArray,$array:ident,$init_val:ident) => {
         unsafe {
@@ -42,15 +27,6 @@ macro_rules! initialize_array {
             .dist_iter_mut()
             .for_each(move |x| *x = $init_val)
             .block();
-    };
-}
-
-macro_rules! onesided_iter {
-    (GlobalLockArray,$array:ident) => {
-        $array.read_lock().block().onesided_iter()
-    };
-    ($arraytype:ident,$array:ident) => {
-        $array.onesided_iter()
     };
 }
 
