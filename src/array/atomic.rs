d2900b1041b71f945d45e571162124a433a53e6f
Ryan D. Friese <ryan.friese@pnnl.gov>
2026-06-13 15:11:15 -0700

    fix docs

--- src/array/atomic.rs
+++ src/array/atomic.rs
@@ -1541,7 +1541,7 @@ impl<T: Dist + AmDist + 'static> AtomicArray<T> {
     /// let array_clone = array.clone();
     /// let _ = array.local_iter().for_each(move |_| {
     ///     let index = rand::thread_rng().gen_range(0..array_clone.len());
-    ///     let _ = array_clone.add(index,1); //randomly at one to an element in the array.
+    ///     let _ = array_clone.add(index,1).spawn(); //randomly at one to an element in the array.
     /// }).block();
     /// world.wait_all();
     /// world.barrier();
@@ -1591,7 +1591,7 @@ impl<T: Dist + AmDist + ElementArithmeticOps + 'static> AtomicArray<T> {
     /// let array_clone = array.clone();
     /// let _ = array.local_iter().for_each(move |_| {
     ///     let index = rand::thread_rng().gen_range(0..array_clone.len());
-    ///     let _ = array_clone.add(index,1); //randomly add one to an element in the array.
+    ///     let _ = array_clone.add(index,1).spawn(); //randomly add one to an element in the array.
     /// }).block();
     /// world.wait_all();
     /// world.barrier();
