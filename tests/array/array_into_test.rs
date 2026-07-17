c189e819881a3e63b6116db6aff9332da37b09c2
Ryan D. Friese <ryan.friese@pnnl.gov>
2026-07-16 22:30:22 -0700

    fix array into test

--- tests/array/array_into_test.rs
+++ tests/array/array_into_test.rs
@@ -1,13 +1,58 @@
 use lamellar::array::prelude::*;
-macro_rules! into_test {
-    ($array1:ident, $array2:ident) => {{
+
+macro_rules! into_test_identity {
+    ($array1:ident) => {{
         let world = lamellar::LamellarWorldBuilder::new().build();
         let _num_pes = world.num_pes();
         let _my_pe = world.my_pe();
 
         let array = $array1::<u32>::new(world.clone(), 1000, Distribution::Block).block();
-        let _array2: $array2<u32> = array.into();
+        let _array2: $array1<u32> = array;
     }};
+}
+
+macro_rules! into_test_conv {
+    ($array1:ident, $array2:ident, $method:ident) => {{
+        let world = lamellar::LamellarWorldBuilder::new().build();
+        let _num_pes = world.num_pes();
+        let _my_pe = world.my_pe();
+
+        let array = $array1::<u32>::new(world.clone(), 1000, Distribution::Block).block();
+        let _array2: $array2<u32> = array.$method().block();
+    }};
+}
+
+macro_rules! into_test {
+    (UnsafeArray, UnsafeArray) => { into_test_identity!(UnsafeArray) };
+    (ReadOnlyArray, ReadOnlyArray) => { into_test_identity!(ReadOnlyArray) };
+    (AtomicArray, AtomicArray) => { into_test_identity!(AtomicArray) };
+    (LocalLockArray, LocalLockArray) => { into_test_identity!(LocalLockArray) };
+    (GlobalLockArray, GlobalLockArray) => { into_test_identity!(GlobalLockArray) };
+
+    (UnsafeArray, ReadOnlyArray) => { into_test_conv!(UnsafeArray, ReadOnlyArray, into_read_only) };
+    (UnsafeArray, AtomicArray) => { into_test_conv!(UnsafeArray, AtomicArray, into_atomic) };
+    (UnsafeArray, LocalLockArray) => { into_test_conv!(UnsafeArray, LocalLockArray, into_local_lock) };
+    (UnsafeArray, GlobalLockArray) => { into_test_conv!(UnsafeArray, GlobalLockArray, into_global_lock) };
+
+    (ReadOnlyArray, UnsafeArray) => { into_test_conv!(ReadOnlyArray, UnsafeArray, into_unsafe) };
+    (ReadOnlyArray, AtomicArray) => { into_test_conv!(ReadOnlyArray, AtomicArray, into_atomic) };
+    (ReadOnlyArray, LocalLockArray) => { into_test_conv!(ReadOnlyArray, LocalLockArray, into_local_lock) };
+    (ReadOnlyArray, GlobalLockArray) => { into_test_conv!(ReadOnlyArray, GlobalLockArray, into_global_lock) };
+
+    (AtomicArray, UnsafeArray) => { into_test_conv!(AtomicArray, UnsafeArray, into_unsafe) };
+    (AtomicArray, ReadOnlyArray) => { into_test_conv!(AtomicArray, ReadOnlyArray, into_read_only) };
+    (AtomicArray, LocalLockArray) => { into_test_conv!(AtomicArray, LocalLockArray, into_local_lock) };
+    (AtomicArray, GlobalLockArray) => { into_test_conv!(AtomicArray, GlobalLockArray, into_global_lock) };
+
+    (LocalLockArray, UnsafeArray) => { into_test_conv!(LocalLockArray, UnsafeArray, into_unsafe) };
+    (LocalLockArray, ReadOnlyArray) => { into_test_conv!(LocalLockArray, ReadOnlyArray, into_read_only) };
+    (LocalLockArray, AtomicArray) => { into_test_conv!(LocalLockArray, AtomicArray, into_atomic) };
+    (LocalLockArray, GlobalLockArray) => { into_test_conv!(LocalLockArray, GlobalLockArray, into_global_lock) };
+
+    (GlobalLockArray, UnsafeArray) => { into_test_conv!(GlobalLockArray, UnsafeArray, into_unsafe) };
+    (GlobalLockArray, ReadOnlyArray) => { into_test_conv!(GlobalLockArray, ReadOnlyArray, into_read_only) };
+    (GlobalLockArray, AtomicArray) => { into_test_conv!(GlobalLockArray, AtomicArray, into_atomic) };
+    (GlobalLockArray, LocalLockArray) => { into_test_conv!(GlobalLockArray, LocalLockArray, into_local_lock) };
 }
 
 macro_rules! match_array2 {
