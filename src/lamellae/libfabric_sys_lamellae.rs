a71af14c0e691dc72d7803e3e21bbe0e33c73ad7
Ryan D. Friese <ryan.friese@pnnl.gov>
2026-08-30 13:47:49 -0700

    wait for background tasks before finalizing libfabric-sys shutdown

--- src/lamellae/libfabric_sys_lamellae.rs
+++ src/lamellae/libfabric_sys_lamellae.rs
@@ -147,8 +147,9 @@ impl LamellaeShutdown for LibfabricSys {
             Ordering::SeqCst,
         );
         // println!("set active to 0");
-        while self.active.load(Ordering::SeqCst) != CmdQStatus::Finished as u8
-            && self.active.load(Ordering::SeqCst) != CmdQStatus::Panic as u8
+        while (self.active.load(Ordering::SeqCst) != CmdQStatus::Finished as u8
+            && self.active.load(Ordering::SeqCst) != CmdQStatus::Panic as u8)
+            || !self.cq.background_tasks_done()
         {
             self.cq.scheduler.exec_task();
         }
